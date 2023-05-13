b55a178f8861169534a177ae2a7f26432c4cacc9
Rasluka <vargklee@hotmail.com>
2023-05-13 17:56:38 -0600

    Adding eslint and prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-}
+  plugins: ['react'],
+  rules: {
+    // quotes: 'off',
+    'react/react-in-jsx-scope': 0,
+  },
+};
